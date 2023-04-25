d9a2c3f91df0f16644bd05fd1486431b5475e518
Sacrmr <sacrmr@gmail.com>
2023-04-25 16:26:00 +0200

    added dictionary component and useStatese

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+      <header className="App-header">Dictionary</header>
+   <main>
+    <Dictionary />
+   </main>
+    </div>
+    <footer className='text-center'>
+      Coded by Samantha Cramer
+    </footer>
     </div>
   );
 }
